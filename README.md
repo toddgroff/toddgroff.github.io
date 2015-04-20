b21497717f857b783389ca58342607eb6d8d9645
Todd Groff <todd.groff@gmail.com>
2015-04-20 17:04:32 -0400

    Update 2015-04-20T21:04:31.447Z

--- README.md
+++
@@ -1,3 +0,0 @@
-## toddgroff.github.io
-
-My portfolio site
---
+++ js/app.js
@@ -0,0 +1,2 @@
+var app={};app.showInitialPage=function(){document.body.insertAdjacentHTML("beforeend","<h2>TODO: Remove example.js</h2><p>And change init.js to do whatevz you need</p>")},app.showInitialPage();
+//# sourceMappingURL=app.js.map
